a15e776ce6459c2e217bc5a9f3425328fb6fd98f
Scott Boggs <scott@tams.tech>
2019-11-05 08:09:03 -0500

    switch from NamedTuple to record

--- src/sysinfo/stat.cr
+++ src/sysinfo/stat.cr
@@ -1,37 +1,39 @@
+require "./info"
+
 module Sysinfo
   # Data read from /proc/stat. See `man 5 proc` on a Linux system.
   class Stat < Info
     getter location : String = "/proc/stat"
 
-    alias CPU = NamedTuple(
-      user: Int64,
-      nice: Int64,
-      system: Int64,
-      idle: Int64,
-      iowait: Int64,
-      irq: Int64,
-      softirq: Int64,
-      steal: Int64,
-      guest: Int64,
-      guest_nice: Int64)
+    record CPU,
+      user : Int64,
+      nice : Int64,
+      system : Int64,
+      idle : Int64,
+      iowait : Int64,
+      irq : Int64,
+      softirq : Int64,
+      steal : Int64,
+      guest : Int64,
+      guest_nice : Int64
 
     # An array of CPUs from an existing stat instance.
     def cpus
       data = File.read("/proc/stat")
       cpus = [] of CPU
       data.scan(/cpu\d\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)/) do |matches|
-        cpu = {
-          user:       matches[1].to_i64,
-          nice:       matches[2].to_i64,
-          system:     matches[3].to_i64,
-          idle:       matches[4].to_i64,
-          iowait:     matches[4].to_i64,
-          irq:        matches[5].to_i64,
-          softirq:    matches[6].to_i64,
-          steal:      matches[7].to_i64,
-          guest:      matches[9].to_i64,
+        cpu = CPU.new(
+          user: matches[1].to_i64,
+          nice: matches[2].to_i64,
+          system: matches[3].to_i64,
+          idle: matches[4].to_i64,
+          iowait: matches[4].to_i64,
+          irq: matches[5].to_i64,
+          softirq: matches[6].to_i64,
+          steal: matches[7].to_i64,
+          guest: matches[9].to_i64,
           guest_nice: matches[10].to_i64,
-        }
+        )
         cpus << cpu
       end
       cpus
